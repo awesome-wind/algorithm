7bfb2f4ab68bf448de684565db4b1300cd8c4779
wangzhang <wangzhang2016@gmail.com>
2017-12-10 19:49:54 +0800

    update

--- coding_interview/src/chapter5/NumberOf1_43.java
+++ coding_interview/src/chapter5/NumberOf1_43.java
@@ -5,45 +5,39 @@ import java.util.Scanner;
 public class NumberOf1_43 {
 
     public static void main(String[] args) {
-        NumberOf1_43 no1 = new NumberOf1_43();
+        NumberOf1_43 no = new NumberOf1_43();
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int result = no1.NumberOf1Between1AndN(n);
+        String num = String.valueOf(n);
+        int result = NumberOf1(num, 0);
         System.out.println(result);
     }
 
-    private int NumberOf1Between1AndN(int n) {
-        if (n <= 0)
+    private static int NumberOf1(String num, int index) {
+        if (num == null || num.length() == 0 || index < 0)
             return 0;
 
-        String num = Integer.toString(n);
-        return NumberOf1(num, 0);
-    }
+        int length = num.length() - index;
 
-    private int NumberOf1(String num, int index) {
-        if (index == num.length() || num.charAt(index) < '0' || num.charAt(index) > '9')
+        if (length == 1 && num.charAt(index) == 0)
             return 0;
-
-        int first = num.charAt(index) - '0';
-        int length = num.length()- index;
-
-        if (length == 1 && first == 0)
-            return 0;
-        if (length == 1 && first > 0)
+        if (length == 1 && num.charAt(index) > 0)
             return 1;
 
-        int numFirstDigit = 0;
+        int first = num.charAt(index) - '0';
+        System.out.println(first);
+        int firstNumbers = 0, otherNumbers, recursiveNumbers;
         if (first > 1)
-            numFirstDigit = (int) Math.pow(10, length - 1);
+            firstNumbers = (int) Math.pow(10, (length - 1));
         else if (first == 1)
-            numFirstDigit = Integer.parseInt(num.substring(index + 1))+1;
+            firstNumbers = Integer.parseInt(num.substring(index + 1)) + 1;
 
-        int numOtherDigits = (int) (first * (length - 1) * Math.pow(10, length - 2));
-        int numRecursive = NumberOf1(num,index+1);
+        otherNumbers = (int) (first * (length - 1) * Math.pow(10, (length - 2)));
+        recursiveNumbers = NumberOf1(num, index + 1);
+        System.out.println(firstNumbers + " " + otherNumbers + " " + recursiveNumbers);
 
-        System.out.println(numFirstDigit+" " +numOtherDigits+" " +numRecursive);
-        return numFirstDigit+numOtherDigits+numRecursive;
+        return firstNumbers + otherNumbers + recursiveNumbers;
     }
 
 
