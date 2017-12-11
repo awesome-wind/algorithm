6187df6c91625b8afcf7fd9c2709b13afa6996e9
wangzhang <wangzhang2016@gmail.com>
2017-12-11 22:33:34 +0800

    add

--- coding_interview/src/Main.java
+++ coding_interview/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        char ch = '0'+49;
+        System.out.println(ch);
     }
 }
---
+++ coding_interview/src/chapter5/TransferDigitsIntoString_46.java
@@ -0,0 +1,50 @@
+package chapter5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TransferDigitsIntoString_46 {
+    public static void main(String[] args) {
+        TransferDigitsIntoString_46 tdi = new TransferDigitsIntoString_46();
+        Scanner sc = new Scanner(System.in);
+        List<List<Character>> list = new ArrayList<>();
+
+        long n = sc.nextInt();
+        String num = String.valueOf(n);
+        int count = tdi.transferDtoS(num);
+        System.out.println(count);
+    }
+
+    private int transferDtoS(String num) {
+        int length = num.length();
+        int[] counts = new int[length];
+        int count = 0;
+
+        for (int i = length - 1; i >= 0; i--) {
+            count = 0;
+
+            if (i < length - 1)
+                count = counts[i + 1];
+            else
+                count = 1;
+
+            if (i < length - 1) {
+                int digit1 = num.charAt(i) - '0';
+                int digit2 = num.charAt(i + 1) - '0';
+                int converted = digit1 * 10 + digit2;
+                if (converted >= 10 && converted <= 26) {
+                    if (i < length - 2)
+                        count += counts[i + 2];
+                    else
+                        count += 1;
+                }
+            }
+            counts[i] = count;
+        }
+        count = counts[0];
+        return count;
+    }
+
+
+}
